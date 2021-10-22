9f104e257f107ebe20262874ba80ee4f2e55bffc
Jeremias <lagoriojeremias@gmail.com>
2021-10-22 10:42:57 -0300

    señales

--- practicas/identificacion_de_sistemas/ejer2.py
+++ practicas/identificacion_de_sistemas/ejer2.py
@@ -1,7 +1,34 @@
+from operator import concat
 import numpy as np
 from scipy import signal
 from akaike import akaike
 from autocorrelacion import autocorrelacion
 from levinson_durbin import levison_durbin as levin
 from matplotlib import pyplot as plt
+from scipy.signal import freqz
+from scipy.fft import fft, ifft, fftfreq
+import math
 
+reader = open("eeg.txt", "r")
+eeg = np.array( [np.double(line.strip()) for line in reader.readlines()] )
+eeg = eeg/max(abs(eeg))
+
+p = 60 
+r = autocorrelacion(eeg, p)
+(A, E) = levin(r, p)
+N = eeg.size
+[I, iMin] = akaike(E, N)
+
+b = [1]
+a = np.concatenate(([1], A[:, iMin-1]))
+w, h = freqz(b, a) 
+fm = 250
+
+SAMPLE_RATE = 1 / N
+yf = fft(eeg)
+xf = fftfreq(N)
+M = N// 2
+print(xf[:M+1])
+plt.plot(xf[:M] , np.abs(yf[:M]))
+plt.plot(w, np.abs(h))
+plt.show()
--- practicas/identificacion_de_sistemas/levinson_durbin.py
+++ practicas/identificacion_de_sistemas/levinson_durbin.py
@@ -1,6 +1,30 @@
 import numpy as np
-
 def levison_durbin(r, p):
+
+    '''
+    Algoritmo de Levison-Durbin para la obtención de los coeficientes a de la ecuación
+    r=Ra
+    
+    Parameters
+    ----------
+    r : numpy array
+        vector de correlación, con un tamaño de p+1
+    p : int 
+        numero de coeficientes a_i que se quieren obtener
+    
+    Returns
+    -------
+    A: numpy matrix
+        matriz con los coeficiente a_i de cada iteración, están ordenado por 
+        columnas, cada columna i tiene los coeficientes del orden i + 1
+    E: numpy array
+        error cuadratico medio total obtenido por las iterracción
+        de tamaño p + 1 E[0] es la autocorrelación sin desplazamiento (Energía)
+    '''
+    
+
+
+
     A = np.zeros((p, p))
     E = np.zeros(p+1)
     k = np.zeros(p)
